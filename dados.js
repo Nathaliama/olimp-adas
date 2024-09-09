f161cd993611c6109634e8510967c20728c6c9a6
Nathaliama <120759763+Nathaliama@users.noreply.github.com>
2024-09-09 17:57:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Raissa Leal, a fadinha, é uma das maiores promessas do skate mundial. Com apenas [idade] anos, ela já conquistou diversos títulos e se tornou um ícone para a nova geração de skatistas.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma das maiorees ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração des brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+    
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. A atleta conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô feminino brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    }
+];
+
